deb05366fba21c1a40c0ee06b82a1a2ffa6396eb
hozza94 <hozza94@users.noreply.github.com>
2020-07-19 00:08:37 +0900

    Update 2020 kakao blind Q3.py
    func add

--- 2020 kakao blind Q3.py
+++ 2020 kakao blind Q3.py
@@ -11,9 +11,12 @@
 #####
 
 ##### 요구사항
+# 3 <= N,M <= 20 의 2차원 배열 (nxn, mxm)
 #####
 
-##### 사용예정인 변수
+##### 사용예정인 함수
+# spin90 (횟수) x 123..
+# move up down left right
 
 ##### 
 
@@ -22,5 +25,8 @@ def solution(key, lock):
     answer = True
     return answer
 
+
+def spin90(time):
+    
 w = input("괄호 문자열 입력(균형잡힌 괄호문자열로 입력!): ")
 print(solution(w))
