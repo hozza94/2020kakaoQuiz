f139814504b2edf02cc684da53860a4b95a7e838
hozza94 <hozza94@users.noreply.github.com>
2020-09-02 02:04:40 +0900

    update kakao q3

--- 2020 kakao blind Q3.py
+++ 2020 kakao blind Q3.py
@@ -59,5 +59,5 @@ def spinList (key):
 
     
 key = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
-lock =  [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
-print(solution(key,lock))
+lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
+print(solution(key, lock))
