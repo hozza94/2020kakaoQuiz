69cc10821b71e257a916ca7343ed3559d55cadb2
hozza94 <hozza94@users.noreply.github.com>
2020-09-02 15:29:13 +0900

    Update 2020 kakao blind Q3.py

--- 2020 kakao blind Q3.py
+++ 2020 kakao blind Q3.py
@@ -32,32 +32,66 @@ for cc in b:
 	for dd in cc:
 		print (dd)
 '''
+import copy
 
 def solution(key, lock):
-    lkey = len(key)
-    llock = len(lock)
+    m = len(key)
+    n = len(lock)
+    t = n + 2*(m-1)
+
+    # keylist = 4가지 회전 포함
     keylist = spinList(key)
-    
+    padd = windowpadd(m, n, lock)
+
     answer = True
-    return answer
+
+    for arr in keylist:
+        for x in range(n+m-1):
+            for y in range(n+m-1):
+                temp = copy.deepcopy(padd)
+                for i in range(m):
+                    for j in range(m):
+                        temp[x+i][y+j] = temp[x+i][y+j] + arr[i][j]
+                if checklock(m, n, temp):
+                    return answer
+
+    return False
 
 def spinList (key):
     keylist = []
-    lkey = len(key)
+    m = len(key)
     slist = key
 
     # 정상, 90, 180, 270 4방향
     keylist.append(key)
-    for m in range(3):
-        temp = [[0 for col in range(lkey)] for row in range(lkey)]
-        for i in range(lkey):
-            for j in range(lkey):
-                temp[j][lkey-1-i] = slist[i][j]
+    for k in range(3):
+        temp = [[0 for col in range(m)] for row in range(m)]
+        for i in range(m):
+            for j in range(m):
+                temp[j][m-1-i] = slist[i][j]
         keylist.append(temp)
         slist = temp
     return keylist
 
-    
+def windowpadd (m, n, lock):
+    # t ,, window padding
+    t = n + 2*(m-1)
+
+    padd = [[0 for col in range(t)] for row in range(t)]
+    for i in range(n):
+        for j in range(n):
+            padd[m-1+i][m-1+j] = lock[i][j]
+
+    return padd
+
+def checklock (m, n, padd):
+    for i in range(n):
+        for j in range(n):
+            if padd[m-1+i][m-1+j] != 1 :
+                return False
+    return True
+
+
 key = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
 lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
 print(solution(key, lock))
